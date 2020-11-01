ef519a13781a0ee8d73c1a4b2d383c1b11e37e1e
vikdoo-d <l>
2020-11-01 17:07:21 +0800

    new file

---
+++ resources/views/genews.blade.php
@@ -0,0 +1,98 @@
+@include('Layout.header')
+<link rel="stylesheet" href="../css/geculture.css">
+<div id="main">
+    <div class="swiper-container swiper-container-horizontal">
+        <div class="swiper-wrapper" style="transform: translate3d(-1903px, 0px, 0px); transition-duration: 300ms;">
+            <div class="swiper-slide swiper-slide-prev" style="width: 1903px;">
+                <img class="banner" src="../img/ge_cul_2.png" alt="">
+            </div>
+            <div class="swiper-slide swiper-slide-active" style="width: 1903px;">
+                <img class="banner" src="../img/ge_cul_1.png" alt="">
+            </div>
+        </div>
+        <div class="swiper-pagination swiper-pagination-clickable swiper-pagination-bullets"><span class="my-bullet" tabindex="0" role="button" aria-label="Go to slide 1"></span><span class="my-bullet my-bullet-active" tabindex="0" role="button" aria-label="Go to slide 2"></span></div>
+        <span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span></div>
+    <div class="icon-block">
+        <div class="icon-block-inner">
+            <div class="block-title">格家文化</div>
+            <div class="block-line"></div>
+            <div class="block-sub-title">成长，让更多人一起成长，是格家的使命</div>
+            <div class="block-sub-title">让我们一起配世界变得更美好，是格家的愿景</div>
+            <div class="block-title-h3">我们的价值观</div>
+            <div class="block-icon-wrap">
+                <img src="http://img.ljcdn.com/midichlorian/80bad3a1-6721-11e9-bef2-c1a07f9a5e49.png" alt="">
+            </div>
+        </div>
+    </div>
+    <div class="icon-block icon-block-first">
+        <div class="icon-block-inner">
+            <div class="block-title">员工活动</div>
+            <div class="block-line"></div>
+        </div>
+        <div class="swiper-container swiper-container-horizontal">
+            <div class="swiper-wrapper" style="transform: translate3d(-5709px, 0px, 0px); transition-duration: 300ms;">
+                <div class="swiper-slide" style="width: 1903px;">
+                    <img class="activity" src="http://img.ljcdn.com/midichlorian/b1d3a401-ab54-11e8-b5f5-55c168a58b42.png" alt="">
+                </div>
+                <div class="swiper-slide" style="width: 1903px;">
+                    <img class="activity" src="http://img.ljcdn.com/midichlorian/b932d901-ab54-11e8-b5f5-55c168a58b42.png" alt="">
+                </div>
+                <div class="swiper-slide swiper-slide-prev" style="width: 1903px;">
+                    <img class="activity" src="http://img.ljcdn.com/midichlorian/c0c37e41-ab54-11e8-b5f5-55c168a58b42.png" alt="">
+                </div>
+                <div class="swiper-slide swiper-slide-active" style="width: 1903px;">
+                    <img class="activity" src="http://img.ljcdn.com/midichlorian/c95abe11-ab54-11e8-b5f5-55c168a58b42.jpeg" alt="">
+                </div>
+            </div>
+            <div class="swiper-pagination swiper-pagination-clickable swiper-pagination-bullets"><span class="my-bullet" tabindex="0" role="button" aria-label="Go to slide 1"></span><span class="my-bullet" tabindex="0" role="button" aria-label="Go to slide 2"></span><span class="my-bullet" tabindex="0" role="button" aria-label="Go to slide 3"></span><span class="my-bullet my-bullet-active" tabindex="0" role="button" aria-label="Go to slide 4"></span></div>
+            <span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span></div>
+    </div>
+    <div class="icon-block">
+        <div class="icon-block-inner">
+            <div class="block-title">逢节必过</div>
+            <div class="block-sub-title">元旦、新年、元宵节、女神节、儿童节、七夕、万圣节、圣诞节……</div>
+            <div class="block-sub-title">只有你想不到的节日，没有我们不过的节日。</div>
+            <div class="block-sub-title">每个节日不仅为大家准备了贴心的礼品，还会有多种活动，给你意外的惊喜。</div>
+        </div>
+        <div class="block-icon-wrap">
+            <img class="img" src="http://img.ljcdn.com/midichlorian/9e9fb771-ab54-11e8-b5f5-55c168a58b42.png" alt="">
+        </div>
+    </div>
+    <div class="icon-block">
+        <div class="icon-block-inner">
+            <div class="block-title">主题生日会</div>
+            <div class="block-sub-title">针对不同月份和星座的小伙伴，会有各种不同主题的趣味生日会，除了party美食，</div>
+            <div class="block-sub-title">还为大家送上贴心的生日礼物，让大家感受到家的温暖，提升幸福感。</div>
+        </div>
+        <div class="block-icon-wrap">
+            <img class="img" src="http://img.ljcdn.com/midichlorian/687809e1-ab54-11e8-b5f5-55c168a58b42.png" alt="">
+        </div>
+    </div>
+    <div class="icon-block">
+        <div class="icon-block-inner">
+            <div class="block-title">主题大餐</div>
+            <div class="block-sub-title">免费三餐之外，每月还为小伙伴准备了不同主题的饕餮大餐。</div>
+            <div class="block-sub-title">世界杯狂欢、海鲜大咖、告别火锅趴、拿手妈妈菜、食草节、</div>
+            <div class="block-sub-title">大饼卷一切、夏天的味道、贴秋膘、咖喱给给等，让大家感受不一样的味蕾盛宴。</div>
+        </div>
+    </div>
+</div>
+
+
+@include('Layout.footer')
+<script>
+    $(document).ready(function () {
+        var mySwiper = new Swiper('.swiper-container', {
+            autoplay: true,
+            delay: 3000,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+                bulletClass: 'my-bullet',
+                bulletActiveClass: 'my-bullet-active',
+            },
+        })
+    })
+</script>
+</body>
+</html>
