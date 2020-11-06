27d8132705ef7bb9c6eb671ce2bd2744f448ed3d
vikdoo-d <derrick_kang@163.com>
2020-11-06 13:27:27 +0800

    add focus module

--- resources/views/gefocus.blade.php
+++ resources/views/gefocus.blade.php
@@ -1,6 +1,29 @@
 @include('Layout.header')
+<link rel="stylesheet" href="../css/focus.css">
+<div id="main">
+    <div class="content">
+        <div class="content-title">关注我们</div>
+        <div class="split-line"></div>
+        <div class="block">
+            <div class="block-item">
+                <img src="../img/email.png" alt="邮箱">
+                <div class="block-title">邮箱</div>
+                <div class="block-desc">derrick_kang@163.com</div>
+            </div>
+            <div class="block-item">
+                <img src="../img/code_zhaopin.png" alt="关注招聘">
+                <div class="block-title">关注招聘</div>
+                <div class="block-desc">格家招聘微信公众号</div>
+            </div>
+            <div class="block-item">
+                <img src="../img/code_guanzhu.png" alt="关注贝壳">
+                <div class="block-title">关注格家</div>
+                <div class="block-desc">格家房产微信公众号</div>
+            </div>
+        </div>
+    </div>
+</div>
 
-
-@include('Layout.footer')
+{{--@include('Layout.footer')--}}
 </body>
 </html>
